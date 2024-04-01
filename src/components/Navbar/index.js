a102a03a75caf2522a58af00c0937319e118637a
vinayjagannavar <vjagannavar@gmail.com>
2024-03-31 23:00:26 -0400

    removed warnings

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,9 +12,9 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+          <NavLink to='/' style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
             <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-          </a>
+          </NavLink>
         </NavLogo>
         <MobileIcon>
           <FaBars onClick={() => {
